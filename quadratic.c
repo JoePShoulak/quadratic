b3914ec6f7f0868d2551d76a64b2887eddb33561
joepshoulak <joepshoulak2@gmail.com>
2014-10-04 21:38:20 -0500

    finished integer formatting

--- quadratic.c
+++ quadratic.c
@@ -57,11 +57,61 @@ int main(int argc, char *argv[]) {
 		char p1[32];
 		char p2[32];
 		
+		int down = 1;
+		
+		if ((int)b == 0) {
+			snprintf(p1, sizeof(p1), "");
+		} else if((int)a == a && (int)b == b) {
+			for (int i=1; i<=(int)((2*a)+b); i++) {
+				if ((int)(2*a/i) == (2*a/i) && (int)(b/i) == (b/i)) {
+					a = a / i;
+					b = b / i;
+					down = down*i;
+					i = 1;
+				}
+			}
+			if (2*a == 1) {
+				snprintf(p1, sizeof(p1), "%d", -(int)b);
+			} else {
+				snprintf(p1, sizeof(p1), "(%d/%d)", -(int)b, (int)(2*a));
+			}
+		} else {
+			snprintf(p1, sizeof(p1), "%g", -b/(2*a));
+		}
+		
+		a = a * down;
+		b = b * down;
+		
 		// Add code for cases where p1 and p2 can be reduced, and reduce them
+		
+		printf("coef: %d\n", (int)coef);
+		printf("2a: %d\n", (int)(2*a));
 
-		snprintf(p1, sizeof(p1), "%g", -b/(2*a));
-		snprintf(p2, sizeof(p2), "%g", coef/(2*a));
-	
+		if ((int)a == a && (int)coef == coef) {
+			printf("1\n");
+			for (int i=1; i<=(int)((2*a)+coef); i++) {
+				printf("2\n");
+				if ((int)(2*a)%i == 0 && (int)(coef) % i == 0) {
+					printf("3\n");
+					a = a / i;
+					coef = coef / i;
+
+					i = 1;
+				}
+			}
+			if (2*a == 1) {
+				if (coef == 1) {
+					snprintf(p2, sizeof(p2), "");
+				} else {
+					snprintf(p2, sizeof(p2), "%d", (int)coef);
+				}
+			} else {
+				snprintf(p2, sizeof(p2), "(%d/%d)", (int)coef, (int)(2*a));
+			}
+		} else {
+			snprintf(p2, sizeof(p2), "%g", coef/(2*a));
+		}
+			
 		int co = 1;
 		if (d<0) {
 			co = -1;
